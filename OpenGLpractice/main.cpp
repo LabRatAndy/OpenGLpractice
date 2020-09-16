6f6d048c916c7cf02e56d925cf09d06dfb2b4050
LabRatAndy <39016522+LabRatAndy@users.noreply.github.com>
2020-09-16 22:15:01 +0100

    test triangle added to main

--- OpenGLpractice/main.cpp
+++ OpenGLpractice/main.cpp
@@ -2,17 +2,26 @@
 #include <GLEW/glew.h>
 #include <GLFW/glfw3.h>
 #include "GLwindow.h"
+#include "Shader.h"
+#include "mesh.h"
+#include "Vertex.h"
+#include<glm/glm.hpp>
 
 int main(int* argc, char* argv)
 {
-	//initialise glfw and glew
+	//initialise glfw
 	glfwInit();
 	std::cout << "hello world" << std::endl;
+	Vertex vertices[] = { Vertex(glm::vec3(-0.5,-0.5,0.0)),Vertex(glm::vec3(0.0,0.5,0.0)),Vertex(glm::vec3(0.5,-0.5,0.0)) };
+	Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]));
+	Shader shader("basicshader.vert", "basicshader.frag");
 	GLwindow theWindow(800, 600, "hello world");
 	while (!theWindow.GetWindowClosing())
 	{
 		glfwPollEvents();
 		theWindow.Clear(0, 150, 50, 255);
+		shader.Use();
+		mesh.Draw();
 		theWindow.UpdateWindow();
 		theWindow.Swapbuffer();
 	}
